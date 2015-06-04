092af5ed7a94597a624fb9e15a58c4bae1ba3c24
seclarke1994 <steven.e.clarke@gmail.com>
2015-06-04 21:36:53 +0300

    v1

---
+++ TestPaste.py
@@ -0,0 +1,8 @@
+'''This another script to test pasting from codecademy'''
+
+
+MY_LIST = [1, 9, 3, 8, 5, 7]
+
+for number in MY_LIST:
+    print 2*number
+    
---
+++ TestingPaste.py
@@ -0,0 +1,12 @@
+'''This script was used to test the viability of pasting from codecademy'''
+def is_even(x_value):
+    '''This is the only submodule'''
+    #This is just a comment#
+    if x_value%2 == 0:
+        return True
+    else:
+        return False
+print is_even(12)
+print 'this is a test string'
+
+
