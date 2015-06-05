41199ab2e33342806c3a2270d3ff37fca173aa7f
seclarke1994 <steven.e.clarke@gmail.com>
2015-06-05 07:52:51 +0300

    v3

---
+++ Exercise C/src/script1.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "seclarke"
+__date__ = "$Jun 1, 2015 8:38:58 PM$"
+
+if __name__ == "__main__":
+    print ("Hello World")
